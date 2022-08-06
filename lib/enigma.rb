4c7c196f7dc46d8d7ae973922028475373847401
Erik Riggs <riggs.erik@gmail.com>
2022-08-06 13:06:55 -0600

    Feat: created encryption method needs a lot of refactoring

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,14 +12,52 @@ class Enigma
 
 
   def encrypt(message, key, date)
-    @key =
-    @date = 
-    shift = @offset_generator.total_offset(@key.key_offset, @date.date_offset)
-    encryption = ""
-    message.split("").each do |letter|
-      if @character_set.include?(letter)
-      end
-
+    if key == nil
+      @key == @key_generator.randomizer
+    else
+      return
     end
+    if date == nil
+      @date == @date_generator.date
+    else
+      return
+    end
+    shift = @offset_generator.total_offset(@key.key_offset, @date.date_offset)
+    require 'pry'; binding.pry
+    encryption = []
+      until message[-1]
+        message.split("").each do |letter|
+        if @character_set.include?(letter.downcase)
+          # encryption += @character_set(@character_set.index(letter) + shift[:a])
+          shifted_characters = shift[a:].times {letter = letter.next}
+          encryption << shifted_characters
+        end
+        message.split("").each do |letter|
+        if @character_set.include?(letter.downcase)
+          # encryption += @character_set(@character_set.index(letter) + shift[:a])
+          shifted_characters = shift[b:].times {letter = letter.next}
+          encryption << shifted_characters
+        end
+        message.split("").each do |letter|
+        if @character_set.include?(letter.downcase)
+          # encryption += @character_set(@character_set.index(letter) + shift[:a])
+          shifted_characters = shift[c:].times {letter = letter.next}
+          encryption << shifted_characters
+        end
+        message.split("").each do |letter|
+        if @character_set.include?(letter.downcase)
+          # encryption += @character_set(@character_set.index(letter) + shift[:a])
+          shifted_characters = shift[d:].times {letter = letter.next}
+          encryption << shifted_characters
+          end
+        end
+        encryption
+      end
+      encrypted_hash = Hash.new(0)
+      encrypted_hash[:encryption] = encryption
+      encrypted_hash[:key] = @key
+      encrypted_hash[:date] = @date
+    end
+    encrypted_hash
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Enigma do
     expect(@enigma.character_set).to eq(("a".."z").to_a)
   end
 
-  xit 'can encrypt' do
+  it 'can encrypt' do
     expected_hash = {
                       encryption: "keder ohulw",
                       key: "02715",
