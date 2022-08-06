b3b1a3567a89436bd688b771abaf5039cc6c733d
Erik Riggs <riggs.erik@gmail.com>
2022-08-05 18:10:04 -0600

    Feat: created a key generator class with randomizer method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,14 @@
 class Enigma
-  def initialize(message)
-    @message = message
-  end 
+  def initialize
+    @character_set = ("a".."z").to_a
+
+  end
+
+
+  def encrypt(message, key, date)
+    cipher_hash = Hash.new(0)
+    cipher_hash[:encryption] = message
+    cipher_hash[:key] = key
+    cipher_hash[:date] = date
+  end
 end
