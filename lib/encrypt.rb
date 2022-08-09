5006539234e2cc5b5ef982a3f0bf4906706f57e3
Erik Riggs <riggs.erik@gmail.com>
2022-08-09 10:06:53 -0600

    encrypt runner read message and return encryption using file i/o

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,15 +2,15 @@ require './lib/enigma'
 require './lib/key_generator'
 require './lib/date_generator'
 require './lib/offset_generator'
+# require 'pry'; binding.pry
+message = File.open(ARGV[0],"r").read
+new_key = KeyGenerator.new
+key = new_key.randomizer
+date = DateGenerator.new.date
 
-message = File.open("./lib/message.txt","r")
-key =  ARGV[1]
-date = ARGV[2]
-
-puts message
-puts key
-puts date
 enigma = Enigma.new(message, key ,date)
 
 
-enigma.encrypt(message, key, date)
+encrypted = enigma.encrypt(message, key, date)
+
+encrypt = File.open(ARGV[1], "w").write(encrypted[:encryption])
