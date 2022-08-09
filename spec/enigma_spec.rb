5cb65f9929269ce49c1e67897d7fb51a294f2ad9
Erik Riggs <riggs.erik@gmail.com>
2022-08-08 20:04:58 -0600

    Test: it can decrypt a message

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,7 +15,13 @@ RSpec.describe Enigma do
     expect(@enigma.message).to eq("hello world")
     expect(@enigma.key).to eq("02715")
     expect(@enigma.date).to eq("040895")
-    expect(@enigma.character_set).to eq(("a".."z").to_a)
+    expect(@enigma.character_set).to eq(("a".."z").to_a << " ")
+  end
+
+  it 'can calculate_shift' do
+    offset = OffsetGenerator.new("02715", "040895")
+    @enigma.calculate_shift("02715", "040895")
+    expect(@enigma.shift).to eq([3, 27, 73, 20])
   end
 
   it 'can encrypt' do
@@ -28,7 +34,7 @@ RSpec.describe Enigma do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected_hash)
   end
 
-  xit 'can decrypt' do
+  it 'can decrypt' do
     expected_hash = {
                       decryption: "hello world",
                       key: "02715",
